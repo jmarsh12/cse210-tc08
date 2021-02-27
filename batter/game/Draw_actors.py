2034f6eed809411c7d7f021f961aca6643e46f9b
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-26 19:28:27 -0700

    Hashed out draw_actors and control_actors. Still need point

--- batter/game/Draw_actors.py
+++ batter/game/Draw_actors.py
@@ -1,3 +1,33 @@
 """
 Jeff
 """
+from game.action import Action
+
+
+class DrawActorsAction(Action):
+    """
+    The purpose of this class is to draw each of the actions of the actors in the cast (paddle, ball, brick)
+    """
+
+    def __init__(self, output_service):
+        """
+        The constructor method.
+
+        Params:
+            output_service (OutputService) an instance of OutputService.
+        """
+        super().__init__()
+        self._output_service = output_service
+
+    def execute(self, cast):
+        """
+        The execute method is meant to actually execute the actions for the actors to perform.
+
+        Params:
+            cast (dictionary)
+        """
+        self._output_service.clear_screen()
+
+        for actor in cast.value():
+            self._output_service.draw_actors(actor)
+        self._output_service.flush_buffer()
--- batter/game/Point.py
+++ batter/game/Point.py
@@ -1,3 +1,23 @@
 """
 Jeff
 """
+
+
+class Point:
+    """
+    The purpose of this class is to set positions for the actors of the code.
+    """
+    def __init__(self, x, y):
+        """
+        The constructor method
+        """
+        self._x = x
+        self._y = y
+
+    def is_equal(self):
+        """
+        TThe is_equal method checks if the position of the ball is equal to the position of the paddle and
+        the bricks.
+        """
+        pass
+    pass
