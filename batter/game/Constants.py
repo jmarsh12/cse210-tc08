f6446a40f4ddd0fcd002745314e52990d4c149cb
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-03-01 13:38:02 -0700

    changed filenames in main

--- batter/game/Constants.py
+++ batter/game/Constants.py
@@ -7,6 +7,5 @@ MAX_X = 80
 MAX_Y = 20
 FRAME_LENGTH = 0.1
 PATH = os.path.dirname(os.path.abspath(__file__))
-MESSAGES = open(PATH + "/messages.txt").read().splitlines()
 ARTIFACTS = 30
 NUM_BALLS = 1
